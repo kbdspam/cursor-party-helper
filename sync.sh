9aa1305648c0b4cc3af28a42351ed98ee872b11a
rtldg <rtldg@protonmail.com>
2024-06-26 15:25:02 +0000

    copy cursors2.js to server stuff

--- sync.sh
+++ sync.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+cp ../cursor-party-rewrite/public/cursors2.js ../cursor-party-server/public
 rsync -trvz --delete \
 	--exclude cursor-party-addon \
+	--exclude cursor-party-rewrite \
 	--exclude='**/node_modules' \
 	--exclude='**/.git' \
 	.. debian@cursor-party-0:/home/debian/c.ookie.click-cursor-party
